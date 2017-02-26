0d7e55ba2aef8f04e7149879acb447003cfb45e3
Evgeny Makashov <mail@spameggs.me>
2017-02-26 18:15:06 +0300

    Rust sum-of-multiples refactor sum to fold

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -3,7 +3,5 @@ fn is_div_any<'a>(num: &'a usize, numbers: &'a Vec<usize>) -> bool {
 }
 
 pub fn sum_of_multiples<'a>(n: usize, numbers: &'a Vec<usize>) -> usize {
-    (1..n).filter(|&x| is_div_any(&x, &numbers)).collect::<Vec<_>>().iter().sum()
-    // variant with fold
-    // (1..n).fold(0, |sum, x| if is_div_any(&x, &numbers) { sum + x } else { sum })
+    (1..n).filter(|&x| is_div_any(&x, &numbers)).fold(0, |sum, x| sum + x)
 }
