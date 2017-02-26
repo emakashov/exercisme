9a3958a83543c95566d02a2cfa25899b734061b5
Evgeny Makashov <mail@spameggs.me>
2017-02-26 18:09:30 +0300

    Rust use fn pow

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,10 @@
 pub fn square_of_sum(n: usize) -> usize {
-    let sum = (1..n + 1).fold(0, |sum, x| sum + x);
-    sum * sum
+    (1..n + 1).fold(0, |sum, x| sum + x).pow(2)
 }
 
 
 pub fn sum_of_squares(n: usize) -> usize {
-    (1..n + 1).fold(0, |sum, x| sum + x * x)
+    (1..n + 1).fold(0, |sum, x| sum + x.pow(2))
 }
 
 
