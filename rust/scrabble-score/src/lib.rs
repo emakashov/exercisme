b371a1d054c2a4f376b368d1888ac626dc29849c
Evgeny Makashov <mail@spameggs.me>
2017-02-28 02:41:28 +0300

    Rust scrabble-score completed

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,16 @@
+fn weight(ch: char) -> usize {
+    match ch {
+        'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+        'D' | 'G'                                                 => 2,
+        'B' | 'C' | 'M' | 'P'                                     => 3,
+        'F' | 'H' | 'V' | 'W' | 'Y'                               => 4,
+        'K'                                                       => 5,
+        'J' | 'X'                                                 => 8,
+        'Q' | 'Z'                                                 => 10,
+        _                                                         => 0,
+    }
+}
+
+pub fn score(word: &str) -> usize {
+    word.to_uppercase().chars().map(|ch| weight(ch)).sum()
+}
