6832d53a04b4b5e26e3b94bfba9974865b79bca0
Evgeny Makashov <mail@spameggs.me>
2017-04-23 15:56:15 +0300

    Rust: luhn without mutables

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,29 +1,17 @@
 pub fn is_valid(string: &str) -> bool {
-    let mut sum: u32 = 0;
-    let mut count: u32 = 0;
+    if string.chars().any(|ch| !(ch.is_digit(10) || ch.is_whitespace())) { return false };
 
-    for (i, ch) in string.chars().rev().filter(|&ch| ch != ' ').enumerate() {
-        let num = match ch.to_digit(10) {
-            Some(n) => n,
-            None => return false,
-        };
-        count += 1;
+    let digits: Vec<u32> = string.chars().filter_map(|ch| ch.to_digit(10)).collect();
 
-        if (i + 1) % 2 == 0 {
-            let prod = num * 2;
-            if prod > 9 {
-                sum += prod / 10 + prod - 10;
-            } else {
-                sum += prod
-            }
-        } else {
-            sum += num
-        }
-    }
+    if digits.len() < 2 { return false };
 
-    if count < 2 {
-        return false
-    }
+    let sum: u32 = digits
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, &num)| if (i + 1) % 2 == 0 { num * 2} else { num })
+        .map(|num| if num > 9 { num / 10 + num - 10 } else { num })
+        .sum();
 
     sum % 10 == 0
 }
